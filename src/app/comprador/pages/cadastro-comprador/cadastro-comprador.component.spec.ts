af6bfe4221f267e74bb0435a3713171e6314e32a
Maurici0M <mauriciomarquescosta7@gmail.com>
2025-04-21 22:53:57 -0300

    draft of the registration page created

---
+++ src/app/comprador/pages/cadastro-comprador/cadastro-comprador.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CadastroCompradorComponent } from './cadastro-comprador.component';
+
+describe('CadastroCompradorComponent', () => {
+  let component: CadastroCompradorComponent;
+  let fixture: ComponentFixture<CadastroCompradorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CadastroCompradorComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(CadastroCompradorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
